55c1a2da5f283d2867aa407c53c706fa9d53587f
SHARUKH-KHAN-77 <sharukh041117@gmail.com>
2026-07-07 20:54:09 +0530

    Add: max-area-of-island - submission-0

---
+++ Data Structures & Algorithms/max-area-of-island/submission-0.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int maxAreaOfIsland(int[][] grid) {
+        int m=grid.length;
+        int n=grid[0].length;
+        int max=0;
+        for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(grid[i][j]==1)
+                {
+                    max=Math.max(max,dfs(grid,i,j));  
+                }
+            }
+        }
+        return max;
+    }
+    public int dfs(int[][] grid,int r,int c)
+    {
+        if(r<0 || c<0 || r>=grid.length || c>=grid[0].length || grid[r][c]==0)
+        {
+            return 0;
+        }
+        grid[r][c]=0;
+        return 1 + dfs(grid,r+1,c) + dfs(grid,r-1,c) + dfs(grid,r,c+1) + dfs(grid,r,c-1);
+    }
+}
